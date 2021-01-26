e98baab3574422498ba7edfdd33e0c30213b8b77
OsamaAalem <osama.aalem@gmail.com>
2021-01-27 00:03:23 +0300

    controller

--- SmartBed/Controller.cs
+++ SmartBed/Controller.cs
@@ -17,21 +17,21 @@ namespace SmartBed
         Motore m;
         public controller(string bed_mov)
         {
-            //sensor sensor = new sensor();
-            //InitializeComponent();
-           
-            //sensor.state(State_bed);
-            //if (State_bed != "u" )
-            //{
-            //    m = new Motore(bed_mov);
-            //}
-            //else if (State_bed !="d")
-            //{
-            //    m = new Motore(bed_mov);
-            //}
-            //else
-            //    MessageBox.Show("the bed in the pleace you want");
-            
+            sensor sensor = new sensor();
+            InitializeComponent();
+
+            sensor.state(State_bed);
+            if (State_bed != "u")
+            {
+                m = new Motore(bed_mov);
+            }
+            else if (State_bed != "d")
+            {
+                m = new Motore(bed_mov);
+            }
+            else
+                MessageBox.Show("the bed in the pleace you want");
+
         }
 
         private void controller_Load(object sender, EventArgs e)
